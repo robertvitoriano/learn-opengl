8732bf4e3508a29cc523cf9f376c11066bf362ce
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-01-06 22:30:05 -0300

    change triangle color

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ const char *fragmentShaderSource = "#version 330 core\n"
                                    "out vec4 FragColor;\n"
                                    "void main()\n"
                                    "{\n"
-                                   "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);;\n"
+                                   "   FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
                                    "}\0";
 
 const char *vertexShaderSource = "#version 330 core\n"
