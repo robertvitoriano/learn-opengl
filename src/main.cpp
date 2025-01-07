11dcd5effa8ddc4ee8a9b955a0d5fd57179480ae
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-01-06 22:41:46 -0300

    drawing rectangle

--- src/main.cpp
+++ src/main.cpp
@@ -188,10 +188,22 @@ void update()
   glDeleteShader(vertexShader);
   glDeleteShader(fragmentShader);
 
-  float triangleVertices[] = {
-      -0.5f, -0.5f, 0.0f,
-      0.5f, -0.5f, 0.0f,
-      0.0f, 0.5f, 0.0f};
+  float vertices[] = {
+      0.5f, 0.5f, 0.0f,   // top right
+      0.5f, -0.5f, 0.0f,  // bottom right
+      -0.5f, -0.5f, 0.0f, // bottom left
+      -0.5f, 0.5f, 0.0f   // top left
+  };
+  unsigned int indices[] = {
+      // note that we start from 0!
+      0, 1, 3, // first triangle
+      1, 2, 3  // second triangle
+  };
+  unsigned int elementBufferObject;
+  glGenBuffers(1, &elementBufferObject);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBufferObject);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
   unsigned int vertexArrayObject, vertexBufferObject;
 
@@ -201,8 +213,9 @@ void update()
   glBindVertexArray(vertexArrayObject);
 
   glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(triangleVertices), triangleVertices, GL_STATIC_DRAW);
-
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBufferObject);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
 
@@ -211,6 +224,8 @@ void update()
   glBindVertexArray(0);
 
   glUseProgram(shaderProgram);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBufferObject);
   glBindVertexArray(vertexArrayObject);
-  glDrawArrays(GL_TRIANGLES, 0, 3);
+  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+  glBindVertexArray(0);
 }
