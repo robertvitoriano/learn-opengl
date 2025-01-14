9b228e351b815bf74c94cea7aef1859fe7f72c36
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-01-13 23:29:06 -0300

    last challenge

--- shaders/fragment_shader.glsl
+++ shaders/fragment_shader.glsl
@@ -1,8 +1,8 @@
 #version 330 core
 out vec4 finalColor;  
 in vec3 ourColor;
-  
+in vec3 outPosition;
 void main()
 {
-    finalColor = vec4(ourColor, 1.0);
+    finalColor = vec4(outPosition, 1.0);
 }
--- shaders/vertex_shader.glsl
+++ shaders/vertex_shader.glsl
@@ -4,8 +4,10 @@ layout (location = 1) in vec3 aColor;
   
 out vec3 ourColor;
 uniform float xOffset;
+out vec3 outPosition;
 void main()
 {
     gl_Position = vec4(aPos.x + xOffset,aPos.y,0.0, 1.0);
     ourColor = aColor;
+    outPosition = aPos;
 }       
